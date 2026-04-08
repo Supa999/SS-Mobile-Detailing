2c51e9cc9b69d8232d8d536072bf72cf80636df2
Supa999 <kevinasteele@icloud.com>
2026-04-07 22:30:40 -0700

    Fixed Facebook logo fill color

--- app/components/landingPage/CTASection.tsx
+++ app/components/landingPage/CTASection.tsx
@@ -6,7 +6,7 @@ export default function CTASection() {
     <section className='min-h-screen py-20 px-8 bg-black flex flex-col items-center justify-center'>
       <div className='w-full flex flex-col items-center justify-center gap-5 bg-linear-to-br from-red-300 to-primary-color rounded-xl p-4'>
         <h2 className='text-4xl font-bold tracking-tighter text-center'>Ready to restore your car to its full potential?</h2>
-        <p className='text-center font-bold'>Reach out to me or book an appointment online?</p>
+        <p className='text-center font-bold'>Reach out to me or book an appointment online!</p>
         <Link href='' className='bg-white text-primary-color p-2 text-xl font-bold tracking-tighter w-full text-center rounded-lg'>Book Appointment</Link>
         <Link href='' className='bg-white text-primary-color p-2 text-xl font-bold tracking-tighter w-full text-center rounded-lg'>Reach out</Link>
       </div>
